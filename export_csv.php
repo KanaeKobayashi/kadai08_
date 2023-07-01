68c70edfdf02633a8e1ccbf816eed0df9ca00b99
T22_かなえ <132175226+KanaeKobayashi@users.noreply.github.com>
2023-07-01 14:44:43 +0900

    ヘッダーミスの修正

--- export_csv.php
+++ export_csv.php
@@ -14,7 +14,7 @@ if ($status == false) {
 } else {
     $file = fopen('php://output', 'w');
     // ヘッダー行を追加
-    fputcsv($file, array('名前', 'Eメール', '本のタイトル', '著者', '評価', 'コメント'));
+    fputcsv($file, array('id','名前', 'Eメール', '本のタイトル', '著者', '評価', 'コメント'));
 
     while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
         // データをCSVファイルに書き込む
